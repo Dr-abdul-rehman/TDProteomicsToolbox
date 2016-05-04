ee4e511391065a076fcd39d7fef26f464bebaace
Biomedical Informatics Research Laboratory <safee.ullah@gmail.com>
2016-05-04 21:34:50 +0500

    Update Return_Protein_MW.m

--- ToolBox/Engine/Return_Protein_MW.m
+++ ToolBox/Engine/Return_Protein_MW.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [computed_mass]=Return_Protein_MW(seq)% function to compute mass of proteins from database
 AA_monoisotopicMass=Monoisotopic_weight;% to get mono-isotopic mass of amino acids
 mass=0.0;
