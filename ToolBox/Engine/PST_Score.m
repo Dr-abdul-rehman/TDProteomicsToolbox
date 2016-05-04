d0efcfcf80ec437c35bbc930a38618196fc2802b
Biomedical Informatics Research Laboratory <safee.ullah@gmail.com>
2016-05-04 21:33:21 +0500

    Update PST_Score.m

--- ToolBox/Engine/PST_Score.m
+++ ToolBox/Engine/PST_Score.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [Final_EST_Score,Tag_indices] = PST_Score(Tags_ladder,Sequence)
 %% Scan database protein sequence for ESTs
     Tag_indices = false;
