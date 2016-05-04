42d686d809a564b78c6754d4dc3fa22cc85de3ba
Biomedical Informatics Research Laboratory <safee.ullah@gmail.com>
2016-05-04 21:30:44 +0500

    Update GetMWofAA.m

--- ToolBox/Engine/GetMWofAA.m
+++ ToolBox/Engine/GetMWofAA.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function dblMWAA = GetMWofAA( strAminoAcid ) 
 %function reading the letter code of a specific amino acid and returing back its MW
     
