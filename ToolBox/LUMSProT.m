1239d11861e8f945f859975aaeb0579bf4ce3d75
Biomedical Informatics Research Laboratory <safee.ullah@gmail.com>
2016-05-04 21:28:19 +0500

    Update LUMSProT.m

--- ToolBox/LUMSProT.m
+++ ToolBox/LUMSProT.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function varargout = LUMSProT(varargin)
 % LUMSPROT MATLAB code for LUMSProT.fignext
 %      LUMSPROT, by itself, creates a toolbarbutton_resetallfields LUMSPROT or raises the existing
