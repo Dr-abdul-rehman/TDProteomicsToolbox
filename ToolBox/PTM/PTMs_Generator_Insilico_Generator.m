c4fcafa81aee212ccad0220d30409870a55121be
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 01:21:11 +0500

    Update PTMs_Generator_Insilico_Generator.m

--- ToolBox/PTM/PTMs_Generator_Insilico_Generator.m
+++ ToolBox/PTM/PTMs_Generator_Insilico_Generator.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function Modified_Protein_Seqs = PTMs_Generator_Insilico_Generator(Candidate_ProteinsList,Protein_sequence,Protein_ExperimentalMW,MWTolerance,Protein_name,Protein_id,PTM_tol,fixed_modifications,variable_modifications,EST_Score)
 
 Modified_Protein_Seqs = {};
