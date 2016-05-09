96631ca6b02c72e21826987890d383d556c7140e
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 01:25:45 +0500

    Update Phosphorylation_S.m

--- ToolBox/PTM/Phosphorylation_S.m
+++ ToolBox/PTM/Phosphorylation_S.m
@@ -1,5 +1,13 @@
-%protein sequence
-%protein_sequence = ['MAPNASCLCVHVRSEEWDLMTFDANPYDSVKKIKEHVRSKTKVPVQDQVLLLGSKILKPRRSLSSYGIDKEKTIHLTLKVVKPSDEELPLFLVESGDEAKRHLLQVRRSSSVAQVKAMIETKTGIIPETQIVTCNGKRLEDGKMMADYGIRKGNLLFLACYCIGG']; %#ok<NBRAK>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function Sites = Phosphorylation_S(protein_sequence, PTM_tolerance)
 
 Sites = {};
