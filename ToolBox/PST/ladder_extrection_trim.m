2bf3c523089cdd9efc0a7f0633441f22c1848a4c
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 02:12:20 +0500

    Update ladder_extrection_trim.m

--- ToolBox/PST/ladder_extrection_trim.m
+++ ToolBox/PST/ladder_extrection_trim.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% This Function is used for the Extrection and the Triming of the Peptide
 %% Sequece tag. It will also remove any duplicate entry present in the list
 function Ladder =  ladder_extrection_trim(Ladder_raw,User_max_TagLength_Threshold)
