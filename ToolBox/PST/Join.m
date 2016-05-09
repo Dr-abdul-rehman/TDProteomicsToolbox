24aeba833730bf87b21a546de4e1cdfac31d19c2
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 01:46:41 +0500

    Update Join.m

--- ToolBox/PST/Join.m
+++ ToolBox/PST/Join.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% This function is used to join the Peptide Sequence tags
 function AA_Next=Join(Home_peak,length_tags,Hop_Info,Num_AAs_Found)
 
