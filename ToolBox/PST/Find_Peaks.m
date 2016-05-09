4ef16577f04ff9e83c61ff61acf5b3e8f7722c18
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 01:38:09 +0500

    Update Find_Peaks.m

--- ToolBox/PST/Find_Peaks.m
+++ ToolBox/PST/Find_Peaks.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function  protsave= findpeaks(AminoAcidTags, AminoAcidMWs, Peak_List)
 function  Hop_Info = Find_Peaks(AminoAcid_Tags, AminoAcid_MWs, Peak_List,User_Hop_Threshold)
 
