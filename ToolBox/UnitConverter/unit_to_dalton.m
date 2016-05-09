dcfbef777997955099ef7d23fadc4c9fa9e674f3
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 00:12:57 +0500

    Update unit_to_dalton.m

--- ToolBox/UnitConverter/unit_to_dalton.m
+++ ToolBox/UnitConverter/unit_to_dalton.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function DaltonValue = unit_to_dalton(value,unit)
 
 if(strcmp(unit,'mmu'))
