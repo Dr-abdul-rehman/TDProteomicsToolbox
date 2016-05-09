c5851845ba3b585aeff16adb84f66465d2acc2fe
zohrabibi <zohra.anwar@gmail.com>
2016-05-10 00:38:14 +0500

    Update Progress_bar.m

--- ToolBox/ResultsDetailedView/Progress_bar.m
+++ ToolBox/ResultsDetailedView/Progress_bar.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           LUMSProT: A MATLAB Toolbox for Top-down Proteomics     %
+%                            Version 1.0.0                         %
+%        Copyright (c) Biomedical Informatics Research Laboratory, %
+%          Lahore University of Management Sciences Lahore (LUMS), %
+%                           Pakistan.                              %
+%                    (http://birl.lums.edu.pk) 	                   %
+%                     (safee.ullah@gmail.com)                      %
+%                    Last Modified on: 4-May-2016                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function varargout = Progress_bar(varargin)
 % PROGRESS_BAR MATLAB code for Progress_bar.fig
 %      PROGRESS_BAR, by itself, creates a new PROGRESS_BAR or raises the existing
