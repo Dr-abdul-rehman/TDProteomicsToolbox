ecb94bf855847fe2f3cbbefe455f3a9994647732
zohrabibi <Zohra@BIO-60318DT.lums.net>
2016-06-15 21:08:50 +0500

    Score

--- ToolBox/Score/Score_Mol_Weight.m
+++ ToolBox/Score/Score_Mol_Weight.m
@@ -17,7 +17,7 @@ function [Candidate_ProteinsList_MW_score]=Score_Mol_Weight(Candidate_ProteinsLi
         if(ERROR==0)
           MW_score=1;   %diff=0 mw_score=1
         else
-        MW_score=10/(ERROR);% compute mol_weight Score for proteins via mw_score=10/|diff|
+        MW_score=1/2^(ERROR);% compute mol_weight Score for proteins via mw_score=10/|diff|
         end
        Candidate_ProteinsList_MW_score{index}{4}=MW_score;%store protein's       
 end
